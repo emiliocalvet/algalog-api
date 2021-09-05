7e8bc9b1da78ff9ecf0380aca47cfc683fda7fe7
Emilio Calvet <emiliocalvet@gmail.com>
2021-09-05 16:03:47 -0300

    adicionado representation model para todo contralador de entregas
    foram adicionadas tanto classes para receber as requisições quanto para enviar as resposta.

--- src/main/java/io/github/emiliocalvet/api/controller/EntregaController.java
+++ src/main/java/io/github/emiliocalvet/api/controller/EntregaController.java
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.emiliocalvet.api.model.DestinatarioModel;
+import io.github.emiliocalvet.api.assembler.EntregaAssembler;
 import io.github.emiliocalvet.api.model.EntregaModel;
+import io.github.emiliocalvet.api.model.input.EntregaInput;
 import io.github.emiliocalvet.domain.model.Entrega;
 import io.github.emiliocalvet.domain.repository.EntregaRepository;
 import io.github.emiliocalvet.domain.service.SolicitacaoEntregaService;
@@ -28,40 +29,26 @@ public class EntregaController {
 	
 	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
+	private EntregaAssembler entregaAssembler;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
-		return solicitacaoEntregaService.solicitar(entrega);
+	public EntregaModel solicitar(@Valid @RequestBody EntregaInput entregaInput) {
+		Entrega novaEntrega = entregaAssembler.toEntity(entregaInput);
+		Entrega entregaSolicitada =	solicitacaoEntregaService.solicitar(novaEntrega);
+		return entregaAssembler.toModel(entregaSolicitada);
 	}
 	
 	
 	@GetMapping
-	public List<Entrega> listar() {
-		return entregaRepository.findAll();
+	public List<EntregaModel> listar() {
+		return entregaAssembler.toCollectionModel(entregaRepository.findAll());
 	}
 	
 	@GetMapping("/{entregaId}")
 	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId) {
 		return entregaRepository.findById(entregaId)
-				.map(entrega -> {
-					EntregaModel entregaModel = new EntregaModel();
-					
-					entregaModel.setId(entrega.getId());
-					entregaModel.setNomeCliente(entrega.getCliente().getNome());
-					entregaModel.setDestinatario(new DestinatarioModel());
-					entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
-					entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-					entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-					entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-					entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-					entregaModel.setTaxa(entrega.getTaxa());
-					entregaModel.setStatus(entrega.getStatus());
-					entregaModel.setDataPedido(entrega.getDataPedido());
-					entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
-					
-					return ResponseEntity.ok(entregaModel);
-				})
+				.map(entrega -> ResponseEntity.ok(entregaAssembler.toModel(entrega)))
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
---
+++ src/main/java/io/github/emiliocalvet/api/model/input/EntregaInput.java
@@ -0,0 +1,26 @@
+package io.github.emiliocalvet.api.model.input;
+
+import java.math.BigDecimal;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntregaInput {
+
+	@NotNull
+	@Valid
+	private ClienteIdInput cliente;
+	
+	@NotNull
+	@Valid
+	private DestinatarioInput destinatario;
+	
+	@NotNull
+	private BigDecimal taxa;
+	
+}
